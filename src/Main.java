35e997b4827dc9b61478b774eb63e38a7095ef30
Derick <dericktavares2006@gmail.com>
2023-09-21 11:01:13 -0300

    derick

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,41 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
         // Classe Objeto         Construtor
         Turista mochileiro = new Turista();
         // Definir formato do objeto
         mochileiro.setNome("Lindsay Lohan");
-        mochileiro.setCpf(Validacao.cpf("123"));
+        mochileiro.setCpf("123");
         Turista mochileira = new Turista("Maria");
         // Definir comportamento
         String retorno = mochileira.viajar();
         System.out.println(retorno);
 
+        // criar um novo objeto
+
+        Turista outroTurista = new Turista();
+        String nome = JOptionPane.showInputDialog("entre com seu nome");
+        outroTurista.setNome(nome);
+        JOptionPane.showInputDialog(null, "O nome digitado foi " + outroTurista.getNome());
+
+        // criar a captura do cpf deste objeto
+        String cpf = JOptionPane.showInputDialog("digite seu cpf:");
+        If(Validacao.cpf(cpf));{
+            outroTurista.setCpf(cpf);
+
+        }
+        els {
+            System.out.println("erro CPF não validado!!!");
+        }
+        outroTurista.setCpf( Validacao.cpf(cpf));
+        JOptionPane.showInputDialog(null, outroTurista.getCpf()+
+                "Validacao");
+
+
+
+    }
+
+    private static void If(String cpf) {
     }
 }
